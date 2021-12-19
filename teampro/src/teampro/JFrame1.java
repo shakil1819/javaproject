3c8ee81423721e3948e14a500249cccb2c5156ea
sayedulabrar <96303632+sayedulabrar@users.noreply.github.com>
2021-12-19 19:23:48 +0600

    muri kha
    2 frame done

--- teampro/src/teampro/JFrame1.java
+++ teampro/src/teampro/JFrame1.java
@@ -58,6 +58,11 @@ public class JFrame1 extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton1.setText("Seat Add");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton2.setText("Book Your Ticket");
@@ -88,6 +93,12 @@ public class JFrame1 extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+               // TODO add your handling code here:
+               seat s=new seat();
+               s.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
